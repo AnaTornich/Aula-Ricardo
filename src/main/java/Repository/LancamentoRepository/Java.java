e719165ae35ceb3cd61b3bc87fe0c42d8534172a
AnaTornich <anasilvatornich@gmail.com>
2025-09-02 20:59:13 -0300

    git commit -m "✨ Feat: Implement CRUD and SPA interface with htmx"

---
+++ src/main/java/Repository/LancamentoRepository/Java.java
@@ -0,0 +1,9 @@
+package Repository.LancamentoRepository;
+
+import br.com.controledegastos.model.Lancamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+}
